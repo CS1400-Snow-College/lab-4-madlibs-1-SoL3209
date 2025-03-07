b723fd1e5778c49e7d97acf8f07f19cf20d5334e
student <student@ENGRD4XD5M3.ad.snow.edu>
2025-03-07 07:50:50 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(@"
+This is Mad Libs. When you are dong providing the requested words
+I will build them into a funny story for you
+Enter the requested word types
+
+Please give me a/an adjective.");
+string adjective1 = Console.ReadLine("");
+Console.WriteLine("Please give me a/an adjective.");
+string adjective2 = 
